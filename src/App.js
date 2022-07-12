ea08ed8d0e2fcc1d603b2a4600e598400d6d30ad
Rosa <humbebrito@yahoo.com>
2022-07-12 17:52:45 -0300

    Adicionando a arquitetura do projeto

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import pdIMG from './assets/pd.png'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+            <span className="login-form-title">Bem Vindo!</span>
+              <span className="login-form-title">
+            <img src={pdIMG} alt="Portal Diagnostico" />
+          </span>
+        </form>
+      </div>
+    </div>
+  </div>
+    
+  );
+}
+export default App;
